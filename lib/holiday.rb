78c234e4fd843593c3bf89bc3fedc73242298b88
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-27 06:05:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,8 +58,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season, values|
-      puts  "#{season}: #{values}"
+    holiday_hash.each do |season, values|
+      puts  "#{season}:#{values.flatten}"
       end
 end
 
